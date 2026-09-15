60608c2297cef0244b9cf47ac4b389a78f651e4f
cmohsinm <cmohsinm@amazon.com>
2026-09-14 20:48:31 -0700

    reset_and_run: forward ACC_CFG (allow zero3_offload for 7B backward-pass OOM)

--- rl_training/rvp_scripts/reset_and_run.sh
+++ rl_training/rvp_scripts/reset_and_run.sh
@@ -19,6 +19,6 @@ done
 # 2) launch the (resumable) flywheel on clean GPUs
 FLYWHEEL="${FLYWHEEL:-math_hard.sh}"
 setsid nohup env BASE="$BASE" BANK="${BANK:-math_full}" EVAL="$EVAL" TAG="$TAG" DPO_BSZ="${DPO_BSZ:-1}" \
-  DPO_MAXLEN="${DPO_MAXLEN:-768}" MAXTOK="${MAXTOK:-1024}" MAXLEN="${MAXLEN:-3072}" NSEED="${NSEED:-3}" \
+  DPO_MAXLEN="${DPO_MAXLEN:-768}" MAXTOK="${MAXTOK:-1024}" MAXLEN="${MAXLEN:-3072}" NSEED="${NSEED:-3}" ACC_CFG="${ACC_CFG:-rl_training/accelerate_zero3.yaml}" \
   bash rl_training/rvp_scripts/$FLYWHEEL > $HOME/gu/logs/${TAG}_run.log 2>&1 &
 echo "[reset] launched $TAG pid $!" >> $HOME/gu/logs/${TAG}_reset.log
